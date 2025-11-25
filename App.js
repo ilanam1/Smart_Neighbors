894235390dfb6e85d8f944dfe9ff3cb29bedf3d5
omerdani <omerdaniel00@gmail.com>
2025-11-25 15:06:18 +0200

    sec commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, StyleSheet, Text, useColorScheme } from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={styles.text}> hello how are you ?</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
+
+export default App;
